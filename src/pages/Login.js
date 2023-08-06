ae79ef161730b572fbc0f64f240b422ad1a68866
felixateya <mulaafelix6@gmail.com>
2023-08-06 22:19:04 +0300

    key changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../images/logo.png"
 function Login() {
   return (
     <div className="Login">
-      <div className="center">
-        <div className="dots">
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
-        </div>
-        <div className="info">
-          <div className="left">
-            <h2>Lions auto</h2>
+          <div className="info">
+            
+            <img src={logo} alt="" />
             <div className="form">
               <h1>Welcome back</h1>
               <p>Please enter your details</p>
@@ -23,11 +17,11 @@ function Login() {
               <label>Password</label>
               <input className="input" type="password" placeholder="Enter your password" />
               <div className="add">
-              <div className="rem">
+              {/* <div className="rem">
               <input type="checkbox"/>
               <p>Remember for 30 days</p>
-              </div>
-              <a href="/">Forgot password</a>
+              </div> */}
+              <a href="/">Forgot password?</a>
               </div>
               <button>Sign in</button>
               <div className="sign">
@@ -35,9 +29,6 @@ function Login() {
               </div>
             </div>
           </div>
-          <div className="right"></div>
-        </div>
-      </div>
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,40 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from "../images/logo.png"
 
 function Register() {
   return (
     <div className="Login">
-      <div className="center">
-        <div className="dots">
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
-        </div>
-        <div className="info">
-          <div className="left">
-            <h2>Lions auto</h2>
+          <div className="info">
+            {/* <h2>Lions auto</h2> */}
+            <img src={logo} alt="" />
             <div className="form">
-              <h1>Welcome</h1>
+              <h1>Welcome back</h1>
               <p>Please enter your details</p>
 
               <label>Name</label>
-
               <input className="input" type="text" placeholder="Enter your name" />
               <label>Email</label>
-
               <input className="input" type="email" placeholder="Enter your email" />
               <label>Password</label>
               <input className="input" type="password" placeholder="Enter your password" />
-              
               <button>Sign up</button>
               <div className="sign">
-                <p>Already have an account? <Link  to="/">Sign in</Link></p>
+                <p><Link to="/">Sign in</Link></p>
               </div>
             </div>
           </div>
-          <div className="right2"></div>
-        </div>
-      </div>
     </div>
   )
 }
